2eb078c7cdc0b6827aa6e68ff163831a67389542
X39 <marco@silipo.de>
2025-11-15 22:49:32 +0100

    separate entity classes into separate files

--- sources/XBridge/Data/BridgeDbContext.cs
+++ sources/XBridge/Data/BridgeDbContext.cs
@@ -9,24 +9,3 @@ public class BridgeDbContext : DbContext
     public DbSet<Project> Projects { get; set; }
     public DbSet<Message> Message { get; set; }
 }
-
-[PrimaryKey(nameof(Id))]
-public class Project
-{
-    public int Id { get; set; }
-    public string Name { get; set; } = null!;
-    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
-    public string? SAPtaskID1  { get; set; }
-    public string? SAPtaskID2 { get; set; }
-
-    public List<Message> Message { get; set; } = new();
-}
-[PrimaryKey(nameof(Id))]
-public class Message
-{
-    public int Id { get; set; }
-    public int ProjectId { get; set; }
-    public Project? Project { get; set; }
-    public string ProjectMessage { get; set; } = null!;
-    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
-}
---
+++ sources/XBridge/Data/Project.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace XBridge.Data;
+
+[PrimaryKey(nameof(Id))]
+public class Project
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = null!;
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    public string? SAPtaskID1  { get; set; }
+    public string? SAPtaskID2 { get; set; }
+
+    public List<Message> Message { get; set; } = new();
+}

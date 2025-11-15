a24d13668dd2dea1bd503d80c9848eb1814c6c37
Alex-Fuh <a.fuhrmann5899@gmail.com>
2025-11-15 21:04:25 +0100

    database

--- Program.cs
+++ Program.cs
@@ -1,7 +1,11 @@
-﻿class Program
-{
-	static void Main(string[] args)
-	{
-		Console.WriteLine("Hello World!");
-	}
-}
+﻿using XBridge.Data;
+
+    var builder = WebApplication.CreateBuilder(args);
+
+    builder.Services.AddDbContext<BridgeDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DbConnectionString"))
+    );
+
+    var app = builder.Build();
+
+    app.Run();
